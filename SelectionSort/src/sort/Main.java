77afb6fe7abcd70d7db3a2ba3d2dd09bf743961b
João Pedro <joao.pedro.azevedo.nascimento@ccc.ufcg.edu.br>
2024-07-23 13:53:14 -0300

    selection sord eda

--- SelectionSort/src/sort/Main.java
+++ SelectionSort/src/sort/Main.java
@@ -1,24 +1,24 @@
 package sort;
 
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		SelectionSort ss = new SelectionSort();
+		//SelectionSort ss = new SelectionSort();
 		Scanner sc = new Scanner(System.in);
 		String[] entrada = sc.nextLine().split(" ");
 		int[] numEntrada = new int[entrada.length];
 		for (int i = 0; i < numEntrada.length; i++) {
 			numEntrada[i] = Integer.parseInt(entrada[i]);
 		}
-		int[] answer = ss.selectionSort(numEntrada);
+		int[] answer = selectionSort(numEntrada);
 		System.out.println(Arrays.toString(answer));
 
 	}
 	
-	/*
+	
 	public static int[] selectionSort(int[] numEntrada) {
 		for (int j = 0; j < numEntrada.length - 1; j++) {
 			int indiceMenor = j;
@@ -33,6 +33,6 @@ public class Main {
 		}
 		return numEntrada;
 	}
-	*/
+	
 	
 }
